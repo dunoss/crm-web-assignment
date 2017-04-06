5d59b3d5106f6d94b1f6417fe92843ad779a1e78
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-04-06 15:51:30 -0400

    delete button works

--- crm.rb
+++ crm.rb
@@ -3,6 +3,7 @@
 # You'll have to implement it from scratch.
 require_relative 'contact'
 require 'sinatra'
+require 'pry'
 
 # Fake data
 # Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
@@ -26,3 +27,17 @@ post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
 end
+
+get '/contacts/:id/delete' do
+  contact_id = params["id"]
+  contact = Contact.find(contact_id.to_i)
+  contact.delete
+  redirect to('/contacts')
+end
+
+Contact.create("Paul", "Lam", "paullamx@gmail.com","Loves biking")
+Contact.create("Harpreet", "Sehmbi", "harpreet@gmail.com","Comedian")
+Contact.create("Kevin", "Liu", "liuke@gmail.com","Vita Mixer")
+Contact.create("Evan", "Tran", "tanni@gmail.com","press 1")
+Contact.create("Maple", "Lam", "maple@gmail.com","Casino Kween")
+Contact.create("Henry", "Lam", "henry@gmail.com","Ping Pong King")
