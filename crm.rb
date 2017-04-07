6e995ff2e92d2c0530d7a79235616516f04f570e
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-04-07 19:55:41 -0400

    404

--- crm.rb
+++ crm.rb
@@ -33,7 +33,11 @@ end
 
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 Contact.create("Paul", "Lam", "paullamx@gmail.com","Loves biking")
