e75acaceff1738cd3ee0532f94a0fbd5401bec9f
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-04-08 18:10:06 -0400

    edit works, update attribute

--- crm.rb
+++ crm.rb
@@ -1,6 +1,3 @@
-# Implement the new web-based CRM here.
-# Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
-# You'll have to implement it from scratch.
 require_relative 'contact'
 require 'sinatra'
 require 'pry'
@@ -28,8 +25,14 @@ post '/contacts' do
   )
   redirect to('/contacts')
 end
+
 # post '/contacts' do
-#   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+#   Contact.create(
+#   params[:first_name],
+#   params[:last_name],
+#   params[:email],
+#   params[:note]
+#   )
 #   redirect to('/contacts')
 # end
 
@@ -62,10 +65,10 @@ end
 put '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
   if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
+    @contact.update_attribute(:first_name, params[:first_name])
+    @contact.update_attribute(:last_name, params[:last_name])
+    @contact.update_attribute(:email, params[:email])
+    @contact.update_attribute(:note, params[:note])
 
     redirect to('/contacts')
   else
@@ -76,11 +79,3 @@ end
 after do
   ActiveRecord::Base.connection.close
 end
-
-
-# Contact.create("Paul", "Lam", "paullamx@gmail.com","Loves biking")
-# Contact.create("Harpreet", "Sehmbi", "harpreet@gmail.com","Comedian")
-# Contact.create("Kevin", "Liu", "liuke@gmail.com","Vita Mixer")
-# Contact.create("Evan", "Tran", "tanni@gmail.com","press 1")
-# Contact.create("Maple", "Lam", "maple@gmail.com","Casino Kween")
-# Contact.create("Henry", "Lam", "henry@gmail.com","Ping Pong King")
